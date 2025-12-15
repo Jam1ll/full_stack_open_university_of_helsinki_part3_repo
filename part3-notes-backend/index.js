e2466ee098cf595c2b4dc65ab59c5d9b464a941d
Jam1ll <jamilguzman202@gmail.com>
2025-12-14 22:16:42 -0400

    changes

--- part3-notes-backend/index.js
+++ part3-notes-backend/index.js
@@ -1,79 +1,119 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//
+// middleware before routes
+//
+
+//in order to deploy correctly
+app.use(express.static("dist"));
+
+morgan.token("body", (req, res) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+//
+// data
+//
 
 let notes = [
   {
-    id: '1',
-    content: 'HTML is easy',
+    id: "1",
+    content: "HTML is easy",
     important: true,
   },
   {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
+    id: "2",
+    content: "Browser can execute only JavaScript",
     important: false,
   },
   {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
     important: true,
   },
-]
+];
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+//
+// endpoints
+//
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find((note) => note.id === id)
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
 
   if (note) {
-    response.json(note)
+    response.json(note);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
 const generateId = () => {
   const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-  return String(maxId + 1)
-}
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
     return response.status(400).json({
-      error: 'content missing',
-    })
+      error: "content missing",
+    });
   }
 
   const note = {
     content: body.content,
     important: body.important || false,
     id: generateId(),
-  }
+  };
 
-  notes = notes.concat(note)
+  notes = notes.concat(note);
 
-  response.json(note)
-})
+  response.json(note);
+});
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter((note) => note.id !== id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-const PORT = 3001
+//
+// port config
+//
+
+//1. to use Fly,io = iwr https://fly.io/install.ps1 -useb | iex
+//2. restart terminal
+//3. fix path = $Env:PATH += ";C:\Users\Lenovo\.fly\bin"
+//4. check file version = fly version
+//5. create an account = fly auth signup
+//6. to start = fly launch
+//*. to edit, edit fly.toml as pleased and then use fly deploy to save changes
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
